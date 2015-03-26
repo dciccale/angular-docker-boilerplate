8d83b65f505f8fabdbac0f6c0a8c05eb0729b588
Denis Ciccale <dciccale@gmail.com>
2015-03-26 16:27:10 +0100

    fix(tests): add missing dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'junit'],
+    reporters: ['progress', 'coverage'],
 
     junitReporter: {
       outputFile: './test/results.xml'
