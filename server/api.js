ed659fd5acca7fef24a16b211956c79d0cb58d6d
Denis Ciccale <dciccale@gmail.com>
2015-12-17 17:12:40 +0100

    fix(proxy): if api proxy is not connected, end request with error

--- server/api.js
+++ server/api.js
@@ -9,9 +9,11 @@ var httpProxy = require('http-proxy');
 module.exports = function (app, config) {
   var apiProxy = httpProxy.createProxyServer();
 
-  apiProxy.on('error', function () {
+  apiProxy.on('error', function (err, req, res) {
     console.log('>> Not connected to the API');
-  })
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.end(err.message);
+  });
 
   app.all('/api/*', function (req, res) {
     req.url = req.url.replace(/\/api/, '');
